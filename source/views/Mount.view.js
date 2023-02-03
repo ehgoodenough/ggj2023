ce0ef56de9fd12d64d089b65963ae358b2f2f5b7
Andrew Goodenough <ehgoodenough@gmail.com>
2023-02-03 18:55:31 +0200

    Added selecting and unselecting the elements.

--- source/views/Mount.view.js
+++ source/views/Mount.view.js
@@ -1,5 +1,6 @@
 import * as Preact from "preact"
 import "views/Mount.view.less"
+import Poin from "poin"
 
 export default class Mount {
     render() {
@@ -14,7 +15,7 @@ export default class Mount {
 class MainScreen {
     render() {
         return (
-            <div class="MainScreen">
+            <div class="MainScreen" onClick={this.onClick}>
                 <div class="CookbookSpace">
                     <div>SOUP????</div>
                     <div>Step 1: Add water to pot.</div>
@@ -27,17 +28,74 @@ class MainScreen {
                     <div>Step 8: Eat!!</div>
                 </div>
                 <div class="CookingSpace">
-                    <div class="CuttingBoard">Cutting Board</div>
-                    <div class="Sink">Sink</div>
-                    <div class="Stove">Stove</div>
-                    <div class="Pot">Pot</div>
-                    <div class="Salt">Salt</div>
-                    <div class="Beef">Beef</div>
-                    <div class="Onions">Onions</div>
-                    <div class="Carrots">Carrots</div>
-                    <div class="Garlic">Garlic</div>
+                    <Item item="CuttingBoard" unselectable={true}/>
+                    <Item item="Sink" unselectable={true}/>
+                    <Item item="Stove" unselectable={true}/>
+                    <Item item="Pot"/>
+                    <Item item="Salt"/>
+                    <Item item="Beef"/>
+                    <Item item="Onions"/>
+                    <Item item="Carrots"/>
+                    <Item item="Garlic"/>
+                    <SelectedItem/>
                 </div>
             </div>
         )
+    }
+    get onClick() {
+        return (event) => {
+            game.selectedItem = undefined
+        }
+    }
+}
+
+class Item {
+    render() {
+        return (
+            <div class={this.class} onClick={this.onClick}
+                style={{"display": game.selectedItem != this.props.item ? "block" : "none"}}>
+                {this.props.item}
+            </div>
+        )
+    }
+    get onClick() {
+        return (event) => {
+            event.stopPropagation()
+            if(game.selectedItem != undefined) {
+
+            }
+
+            if(this.props.unselectable == true) return
+            game.selectedItem = this.props.item
+        }
+    }
+    get class() {
+        return ["Item", this.props.item, "atEase"].join(" ")
+    }
+}
+
+class SelectedItem {
+    render() {
+        if(game.selectedItem == undefined) return
+        return (
+            <div class={"SelectedItem" + " " + game.selectedItem} style={this.style}>
+                {game.selectedItem}
+            </div>
+        )
+    }
+    get style() {
+        return {
+            "left": Poin.position.x + "px",
+            "top": Poin.position.y + "px",
+        }
+    }
+}
+
+const game = {
+    "selectedItem": undefined,
+    "items": {
+        "CuttingBoard": {
+
+        }
     }
 }
