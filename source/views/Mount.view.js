2eac3ffd1e82ebfd1c2e86b497cc20eef201e5ea
Andrew Goodenough <ehgoodenough@gmail.com>
2023-02-03 17:20:28 +0200

    Added a bunch of elements.

--- source/views/Mount.view.js
+++ source/views/Mount.view.js
@@ -4,7 +4,7 @@ import "views/Mount.view.less"
 export default class Mount {
     render() {
         return (
-            <div className="Mount" onDrop={this.onDrop}>
+            <div className="Mount">
                 <MainScreen/>
             </div>
         )
@@ -15,7 +15,28 @@ class MainScreen {
     render() {
         return (
             <div class="MainScreen">
-                Hello World!!
+                <div class="CookbookSpace">
+                    <div>SOUP????</div>
+                    <div>Step 1: Add water to pot.</div>
+                    <div>Step 2: Put pot on stove. Boil.</div>
+                    <div>Step 3: Cut onions, carrots and garlic. Put in pot.</div>
+                    <div>Step 4: Stir.</div>
+                    <div>Step 5: Cut beef into tiny(?) cubes. Put in the pot.</div>
+                    <div>Step 6: Stir.</div>
+                    <div>Step 7: Add salt, to taste.</div>
+                    <div>Step 8: Eat!!</div>
+                </div>
+                <div class="CookingSpace">
+                    <div class="CuttingBoard">Cutting Board</div>
+                    <div class="Sink">Sink</div>
+                    <div class="Stove">Stove</div>
+                    <div class="Pot">Pot</div>
+                    <div class="Salt">Salt</div>
+                    <div class="Beef">Beef</div>
+                    <div class="Onions">Onions</div>
+                    <div class="Carrots">Carrots</div>
+                    <div class="Garlic">Garlic</div>
+                </div>
             </div>
         )
     }
